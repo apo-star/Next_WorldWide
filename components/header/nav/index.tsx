3ba2be86026d6b33853b5dfcd52278fb110f9455
apo-star <apolle.world@gmail.com>
2024-04-05 18:11:57 +0800

    remove pathname

--- components/header/nav/index.tsx
+++ components/header/nav/index.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 // import styles from "./style.module.css";
 import { motion } from "framer-motion";
-import { usePathname } from "next/navigation";
 import { menuSlide } from "../anim";
 import Curve from "./Curve";
 import Footer from "./Footer";
 
 export default function index() {
-	const pathname = usePathname();
-	const [selectedIndicator, setSelectedIndicator] = useState(pathname);
-
 	return (
 		<motion.div
 			variants={menuSlide}
@@ -19,12 +15,7 @@ export default function index() {
 			className="fixed top-0 right-0 h-screen bg-gray-900 text-white"
 		>
 			<div className="flex flex-col justify-between h-full p-20">
-				<div
-					onMouseLeave={() => {
-						setSelectedIndicator(pathname);
-					}}
-					className="flex flex-col"
-				>
+				<div className="flex flex-col">
 					<div className="text-xl text-gray-600 uppercase border-b border-gray-600 mb-4">
 						Navigation
 					</div>
